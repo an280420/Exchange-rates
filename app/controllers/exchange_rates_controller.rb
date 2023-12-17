04135a4b375b5fbbdb83f14cf78934eee875f028
Anton Nekrasov <antonnekrasov82@gmail.com>
2023-12-17 20:39:04 +0500

    data prepare for charts

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,7 +1,37 @@
 class ExchangeRatesController < ApplicationController
   def chart
+    @chart_data = chart_data
   end
 
   def table
   end
+
+  private
+
+  def chart_data
+    rates = Rate.includes(:currency).where('date >= ?', 1.month.ago).order(:currency_id)
+    rates_group_by_date = rates.group_by(&:date)
+    
+    # Добавим даты в массив
+    dates = ['x'] + rates_group_by_date.keys.map(&:to_s)
+  
+    columns = []
+    columns << dates
+  
+    # Добавим данные по каждой валюте
+    Currency.all.each do |currency|
+      column_data = [currency.code]
+  
+      rates_for_currency = rates_group_by_date.map { |date, rates| rates.find { |rate| rate.currency_id == currency.id } }
+      column_data += rates_for_currency.map { |rate| rate ? rate.value : nil }
+  
+      columns << column_data
+    end
+  
+    { 
+      x: 'x',
+      columns: columns
+    }
+  end
+  
 end
--- app/javascript/chart.js
+++ app/javascript/chart.js
@@ -2,17 +2,16 @@ import * as d3 from 'd3';
 import c3 from "c3";
 
 document.addEventListener('DOMContentLoaded', function() {
-  const chartData = {
-    columns: [
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 50, 20, 10, 40, 15, 25]
-    ]
-  };
-
   if (document.getElementById('chart')) {
     c3.generate({
       bindto: '#chart',
-      data: chartData
+      data: chartData,
+      axis: {
+        x: {
+            type: 'timeseries',
+            tick: {format: '%Y-%m-%d'}
+        }
+      }
     });
   } else {
     console.error("Element #chart not found");
