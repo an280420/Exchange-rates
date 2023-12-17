dbd2c988de26a699152367c731095f0a6568c4dc
Anton Nekrasov <antonnekrasov82@gmail.com>
2023-12-17 14:53:55 +0500

    created db schema

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_091051) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "code", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_currencies_on_code", unique: true
+  end
+
+  create_table "rates", force: :cascade do |t|
+    t.bigint "currency_id", null: false
+    t.decimal "value"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["currency_id"], name: "index_rates_on_currency_id"
+    t.index ["date", "currency_id"], name: "index_rates_on_date_and_currency_id", unique: true
+  end
+
+  add_foreign_key "rates", "currencies"
+end
