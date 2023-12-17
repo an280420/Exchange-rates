5dee2ac0775159793a0d75b99794d5f6601e588c
Anton Nekrasov <antonnekrasov82@gmail.com>
2023-12-17 21:13:01 +0500

    added prepare data for rates table

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -4,6 +4,7 @@ class ExchangeRatesController < ApplicationController
   end
 
   def table
+    @table_data = table_data
   end
 
   private
@@ -12,13 +13,11 @@ class ExchangeRatesController < ApplicationController
     rates = Rate.includes(:currency).where('date >= ?', 1.month.ago).order(:currency_id)
     rates_group_by_date = rates.group_by(&:date)
     
-    # Добавим даты в массив
     dates = ['x'] + rates_group_by_date.keys.map(&:to_s)
   
     columns = []
     columns << dates
   
-    # Добавим данные по каждой валюте
     Currency.all.each do |currency|
       column_data = [currency.code]
   
@@ -33,5 +32,30 @@ class ExchangeRatesController < ApplicationController
       columns: columns
     }
   end
-  
+
+  def table_data
+    rates = Rate.includes(:currency).where('date >= ?', 4.weeks.ago).order(:currency_id, :date)
+
+    currencies_data = {}
+
+    rates.each do |rate|
+      currencies_data[rate.currency_id] ||= { currency: rate.currency, data: [] }
+      currencies_data[rate.currency_id][:data] << { date: rate.date, value: rate.value }
+    end
+
+    table_rows = []
+
+    currencies_data.each do |currency_id, data|
+      row = [data[:currency].code]
+
+      data[:data].each_cons(2) do |prev_week, current_week|
+        difference_percentage = ((current_week[:value] - prev_week[:value]) / prev_week[:value] * 100).round(2)
+        row << "#{current_week[:value].round(2)}₽ (#{difference_percentage}%)"
+      end
+
+      table_rows << row
+    end
+
+    table_rows
+  end
 end
